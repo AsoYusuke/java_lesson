9a0462b1e18bf0d040a6c772f4a1a5b1cbfcea52
浅生裕介 <asopc@asouyuusukenoMacBook-Air.local>
2021-07-04 23:13:59 +0900

    add

---
+++ fizzbuzz/src/fizzbuzz/practice.java
@@ -0,0 +1,10 @@
+package fizzbuzz;
+
+public class practice {
+	public static void main(String[] args) {
+		System.out.println("おはよう");
+		System.out.println("こんにちは");
+		System.out.println("こんばんは");
+		
+	}
+}
