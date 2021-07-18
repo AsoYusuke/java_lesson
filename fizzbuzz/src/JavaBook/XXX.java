5ff1c6744b600514b887946f3619e999e77a6883
AsoYusuke <aso.aso.1227@i.softbank.jp>
2021-07-18 23:38:48 +0900

    add

--- fizzbuzz/src/JavaBook/XXX.java
+++ fizzbuzz/src/JavaBook/XXX.java
@@ -2,13 +2,13 @@ package JavaBook;
 
 public class XXX {
 	public static void main(String[] args) {
-		for (int i = 1; i < 6; i++) {
+		for (int i = 1; i <= 5; i++) {
 			if (i == 1 || i == 5) {
 				System.out.println("-----");
 			} else if (i % 2 == 0) {
 				System.out.println("  |");
 			} else {
-				System.out.println("  ---");
+				System.out.println("  |--");
 			}
 		}
 	}
